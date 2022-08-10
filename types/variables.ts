6794d3d28a429d917a6e65d0b694faad75af632a
luohao <1223048059@qq.com>
2022-08-10 22:24:19 +0800

    add some

--- types/variables.ts
+++ types/variables.ts
@@ -1,7 +1,19 @@
-let age: number = 22;
 let username: string = "John";
-let flag: boolean = true;
+let age: number = 22;
+let mail: boolean = true;
 // let bigNum: bigint = 12345n;
-let n: null = null;
-let u: undefined = undefined;
+let girlfriend: null = null;
+let girlfriendsName: undefined = undefined;
 let sym: symbol = Symbol("id");
+let userinfo: {
+  address: string;
+  phoneNumber: number;
+} = {
+  address: "123 Main St",
+  phoneNumber: 1234567890,
+};
+let id: string | number = 123;
+
+function compare(a: number, b: number): 1 | 0 | -1 {
+  return a === b ? 0 : a > b ? 1 : -1;
+}
